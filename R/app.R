a0920c06cc0fae83e7f2f7b63957ba2e52601ab9
Russ Hyde <russ.hyde.data@gmail.com>
2021-07-17 21:21:45 +0100

    [chk] wrap colnames in .data (so R CMD check doesn't complain about missing vars)

--- R/app.R
+++ R/app.R
@@ -30,29 +30,29 @@ er_app <- function() {
 
   server <- function(input, output, session) {
 
-    selected <- reactive(injuries %>% dplyr::filter(prod_code == input$code))
+    selected <- reactive(injuries %>% dplyr::filter(.data[["prod_code"]] == input$code))
 
     output$diag <- renderTable(
-      selected() %>% dplyr::count(diag, wt = weight, sort = TRUE)
+      selected() %>% dplyr::count(.data[["diag"]], wt = .data[["weight"]], sort = TRUE)
     )
     output$body_part <- renderTable(
-      selected() %>% dplyr::count(body_part, wt = weight, sort = TRUE)
+      selected() %>% dplyr::count(.data[["body_part"]], wt = .data[["weight"]], sort = TRUE)
     )
     output$location <- renderTable(
-      selected() %>% dplyr::count(location, wt = weight, sort = TRUE)
+      selected() %>% dplyr::count(.data[["location"]], wt = .data[["weight"]], sort = TRUE)
     )
 
     summary <- reactive({
       selected() %>%
-        dplyr::count(age, sex, wt = weight) %>%
+        dplyr::count(.data[["age"]], .data[["sex"]], wt = .data[["weight"]]) %>%
         dplyr::left_join(population, by = c("age", "sex")) %>%
-        dplyr::mutate(rate = n / population * 1e4)
+        dplyr::mutate(rate = .data[["n"]] / .data[["population"]] * 1e4)
     })
 
     output$age_sex <- renderPlot(
       {
         summary() %>%
-          ggplot2::ggplot(ggplot2::aes(age, n, colour = sex)) +
+          ggplot2::ggplot(ggplot2::aes(.data[["age"]], .data[["n"]], colour = .data[["sex"]])) +
           ggplot2::geom_line() +
           ggplot2::labs(y = "Estimated number of injuries")
       },
