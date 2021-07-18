d84aefa24bb76a383bd89266e9feb995c8f79af7
Russ Hyde <russ.hyde.data@gmail.com>
2021-07-18 21:15:33 +0100

    [rx-test] separate ui & server functions from 'er_app'

--- R/app.R
+++ R/app.R
@@ -9,6 +9,17 @@ er_app <- function() {
   products <- get("products")
   population <- get("population")
 
+  ui <- er_ui(products = products)
+
+  server <- purrr::partial(
+    er_server,
+    injuries = injuries, products = products, population = population
+  )
+
+  shiny::shinyApp(ui, server)
+}
+
+er_ui <- function(products) {
   prod_codes <- stats::setNames(products$prod_code, products$title)
 
   ui <- fluidPage(
@@ -28,36 +39,41 @@ er_app <- function() {
     )
   )
 
-  server <- function(input, output, session) {
-    selected <- reactive(injuries %>% dplyr::filter(.data[["prod_code"]] == input$code))
+  ui
+}
 
-    output$diag <- renderTable(
-      count_by_weight(selected(), "diag")
-    )
-    output$body_part <- renderTable(
-      count_by_weight(selected(), "body_part")
-    )
-    output$location <- renderTable(
-      count_by_weight(selected(), "location")
-    )
+er_server <- function(input,
+                      output,
+                      session,
+                      injuries,
+                      products,
+                      population) {
+  selected <- reactive(injuries %>% dplyr::filter(.data[["prod_code"]] == input$code))
 
-    summary <- reactive({
-      selected() %>%
-        dplyr::count(.data[["age"]], .data[["sex"]], wt = .data[["weight"]]) %>%
-        dplyr::left_join(population, by = c("age", "sex")) %>%
-        dplyr::mutate(rate = .data[["n"]] / .data[["population"]] * 1e4)
-    })
+  output$diag <- renderTable(
+    count_by_weight(selected(), "diag")
+  )
+  output$body_part <- renderTable(
+    count_by_weight(selected(), "body_part")
+  )
+  output$location <- renderTable(
+    count_by_weight(selected(), "location")
+  )
 
-    output$age_sex <- renderPlot(
-      {
-        summary() %>%
-          ggplot2::ggplot(ggplot2::aes(.data[["age"]], .data[["n"]], colour = .data[["sex"]])) +
-          ggplot2::geom_line() +
-          ggplot2::labs(y = "Estimated number of injuries")
-      },
-      res = 96
-    )
-  }
+  summary <- reactive({
+    selected() %>%
+      dplyr::count(.data[["age"]], .data[["sex"]], wt = .data[["weight"]]) %>%
+      dplyr::left_join(population, by = c("age", "sex")) %>%
+      dplyr::mutate(rate = .data[["n"]] / .data[["population"]] * 1e4)
+  })
 
-  shiny::shinyApp(ui, server)
+  output$age_sex <- renderPlot(
+    {
+      summary() %>%
+        ggplot2::ggplot(ggplot2::aes(.data[["age"]], .data[["n"]], colour = .data[["sex"]])) +
+        ggplot2::geom_line() +
+        ggplot2::labs(y = "Estimated number of injuries")
+    },
+    res = 96
+  )
 }
